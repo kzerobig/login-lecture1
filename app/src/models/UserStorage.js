ae6868e222ecbe2c8b836b431380b95afb2b78ea
daeyoung <zerobig7713@gmail,com>
2025-01-23 18:07:17 +0900

    로그인 화면 꾸미기

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,10 +1,11 @@
-"use strict"
+"use strict";
 class UserStorage {
     static #users = {              //#은 은닉화를 도와줌
         id : ["rlaeodud"],
         psword : ["1234"],
         nams : ["김대영"],
     };
+
     static getUsers(...fields){
         const users = this.#users;
         const newUsers = fields.reduce((newUsers, field)=>{
@@ -16,5 +17,15 @@ class UserStorage {
         
         return newUsers;
     }
-};
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const userKeys = Object.keys(users) // => [id,psword,name]
+        const userInfo = userKeys.reduce((newUser, info)=>{
+            newUser[info] = users[info][idx];
+            return newUser;
+        },{});
+        return userInfo;
+    }
+}
 module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,7 @@
 "use strict";
-
+const User = require("../../models/User"); 
 const UserStorage = require("../../models/UserStorage");
+
 //컨트롤러 
 
 const output = {
@@ -17,24 +18,30 @@ const output = {
 
 const process = {
     login: (req,  res) => {
-        const id = req.body.id,
-            psword = req.body.psword;
+        const user = new User(req.body);
+        const response = user.login();
+        console.log(response);
+    //return res.json(response);
+
+    //     const id = req.body.id,
+    //         psword = req.body.psword;
 
         
-        const users = UserStorage.getUsers("id","psword");
+    //     const users = UserStorage.getUsers("id","psword");
 
-         const response = {};
+    //      const response = {};
  
-        if (users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if (users.psword[idx] === psword) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다."
-        return res.json(response);
+    //     if (users.id.includes(id)){
+    //         const idx = users.id.indexOf(id);
+    //         if (users.psword[idx] === psword) {
+    //             response.success = true;
+    //             return res.json(response);
+    //         }
+    //     }
+    //     response.success = false;
+    //     response.msg = "로그인에 실패하셨습니다."
+    //     return res.json(response);
+    
     },
 };
 
